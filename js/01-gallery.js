ec8b98ad810486159ac3bfe54433c1ca538c17ed
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2023-10-09 01:09:59 +0300

    remove EventListener onClose modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,7 +19,14 @@ let instance = null;
 function handleGalleryClick(event) {
     event.preventDefault();
     if (event.target.nodeName !== 'IMG') return;
-    instance = basicLightbox.create(`<img src=${event.target.dataset.source} alt=${event.target.alt}>`);
+    instance = basicLightbox.create(`<img src=${event.target.dataset.source} alt=${event.target.alt}>`, {
+        onShow: (instance) => {
+            document.addEventListener('keydown', handleKeydownEscape);
+        },
+        onClose: (instance) => {
+            document.removeEventListener('keydown', handleKeydownEscape);
+        }
+    });
     instance.show();
 }
 
@@ -29,12 +36,15 @@ galleryRef.addEventListener('click', handleGalleryClick);
 
 /**
  * handle keydown Escape
+ * @param event
  */
-document.addEventListener('keydown', event => {
+function handleKeydownEscape(event) {
     if (instance) {
         if (event.key === 'Escape') {
             instance.close();
             instance = null;
         }
     }
-});
+}
+
+
